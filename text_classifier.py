39d711a68d2bdfa4fb591849690c74a9165650b0
Zanderax <davidjamesbelcher0@gmail.com>
2018-05-22 02:24:32 +0000

    Ceaned up the code to make it more readable and useable

--- text_classifier.py
+++ text_classifier.py
@@ -5,30 +5,28 @@ import numpy as np
 import os
 import pandas as pd
 import re
+import shutil
+
 # import seaborn as sns
 
-import csv
+import argparse
 
-score = "sNEU"
+import csv
+tempDir = os.environ['TMPDIR']
+
+validScores = ["sNEU", "sAGR", "sCON", "sOPN", "sEXT" ]
+validModules = ["https://tfhub.dev/google/nnlm-en-dim128/1"]
 
 # Load all files from a directory in a DataFrame.
-def load_facebook_data():
+def load_facebook_data( score ):
     data = {}
     data["status"] = []
     data[score] = []
-    # data["neu"] = []
-    # data["agr"] = []
-    # data["con"] = []
-    # data["opn"] = []
     with open('facebook_clean.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             data["status"].append(row["STATUS"])
             data[score].append(int(float(row[score])) )
-            # data["neu"].append(row["sNEU"])
-            # data["agr"].append(row["sAGR"])
-            # data["con"].append(row["sCON"])
-            # data["opn"].append(row["sOPN"])\
     train = {}
     train["status"] = data["status"][:9000]
     train[score] =  data[score][:9000]
@@ -37,41 +35,76 @@ def load_facebook_data():
     test["status"] = data["status"][9001:]
     test[score] =  data[score][9001:]
     return pd.DataFrame.from_dict(train), pd.DataFrame.from_dict(test), test
-    
-if __name__ == "__main__":
+
+
+def RunModel( score, module ):
+    # We only care about errors
     tf.logging.set_verbosity(tf.logging.ERROR)
-    train_df, test_df, test = load_facebook_data()
+    
+    # Set up our data frames
+    train_df, test_df, test = load_facebook_data(score)
     train_df.head()
+    
+    #
     train_input_fn = tf.estimator.inputs.pandas_input_fn(
     train_df, train_df[score], num_epochs=None, shuffle=True)
     predict_train_input_fn = tf.estimator.inputs.pandas_input_fn(
     train_df, train_df[score], shuffle=False)
     predict_test_input_fn = tf.estimator.inputs.pandas_input_fn(
     test_df, test_df[score], shuffle=False)
+    
+    # Set up our feature columns from the model
     embedded_text_feature_column = hub.text_embedding_column(
-    key="status", 
-    module_spec="https://tfhub.dev/google/nnlm-en-dim128/1",)
+        key="status", 
+        module_spec=module,)
+        
+    # Set up our neural network 
     estimator = tf.estimator.DNNClassifier(
-    hidden_units=[500, 100],
-    feature_columns=[embedded_text_feature_column],
-    n_classes=6,
-    optimizer=tf.train.AdagradOptimizer(learning_rate=0.003,))
-    estimator.train(input_fn=train_input_fn, steps=10);
+        hidden_units=[500, 100],
+        feature_columns=[embedded_text_feature_column],
+        n_classes=6,
+        optimizer=tf.train.AdagradOptimizer(learning_rate=0.003,))
+        
+    # Train our neural network
+    estimator.train(input_fn=train_input_fn, steps=10)
+    
+    # Get Results
     train_eval_result = estimator.evaluate(input_fn=predict_train_input_fn)
     test_eval_result = estimator.evaluate(input_fn=predict_test_input_fn)
     test_predictions = estimator.predict(input_fn=predict_test_input_fn)
-    #print "Test type = " + str(type(test))
-    print "test score type = " + str(type(test[score]))
-    # print "test_predictions dir = " + str(dir(test_predictions))
-    # print "gi_code type = " + str(type(test_predictions.gi_code))
-    # print "gi_frame type = " + str(type(test_predictions.gi_frame))
-    # print "gi_running type = " + str(type(test_predictions.gi_running))
-    #rmse = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(test[score], test_predictions))))
-    scoreTensor = tf.convert_to_tensor( test[score])
-    # mse = tf.metrics.mean_squared_error(scoreTensor, test_predictions.gi_running )
     
-    #tf.metrics.mean_squared_error(
-    print "Training set accuracy: {accuracy}".format(**train_eval_result)
-    print "Test set accuracy: {accuracy}".format(**test_eval_result)
-    #print "RMSE = {rmse}".format(rmse)
-    #print "MSE Type = {mse}".format(type(mse))
+    train_accuracy = train_eval_result["accuracy"] * 100.
+    test_accuracy = test_eval_result["accuracy"] * 100.
+    
+    
+    # Print results
+    print "Results for " + score
+    print "================"
+    print "Training set accuracy: {0:.2f}%".format(train_accuracy)
+    print "Test set accuracy: {0:.2f}%".format(test_accuracy)
+    print ""
+    
+def ParseArgs():
+    parser = argparse.ArgumentParser(description='Model Arguments')
+    parser.add_argument('--scores', help='The score to use', choices=validScores, default=validScores[0], nargs='*')
+    parser.add_argument('--module', help='The module to use', choices=validModules, default=validModules[0])
+    return parser.parse_args()
+    
+def ClearTmpDir():
+    fileList = os.listdir(tempDir)
+    for fileName in fileList:
+        shutil.rmtree(tempDir+"/"+fileName)
+    
+def main():
+    args = ParseArgs()
+    scores = args.scores
+    module = args.module
+    ClearTmpDir()
+    for score in scores:
+        RunModel( score, module )
+        ClearTmpDir()
+       
+    
+if __name__ == "__main__":
+    main()
+    
